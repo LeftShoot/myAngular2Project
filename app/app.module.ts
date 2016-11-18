0866c44f7f351281cc1a2a7fd5cb5dff8d704291
yyang2 <yyang2@ewaytec.cn>
2016-11-18 18:38:13 +0800

    增加http请求

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,12 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
@@ -17,6 +23,8 @@ import { AppRoutingModule }     from './app-routing.module'; //路由模块
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [
